c2fcde50877048bb8f99aa9f69c05b1296380d9f
mustafafromearth <62035667+mustafafromearth@users.noreply.github.com>
2025-10-26 18:10:38 +0530

    make major changes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const Digits = document.querySelectorAll(".digit")
 const Operators = document.querySelectorAll(".operator")
+const Equals = document.getElementById("equals")
 const OutputDisplay = document.getElementById("output-display")
 const AllClear = document.getElementById("all-clear")
 const UpperDisplay = document.getElementById("calc-upper-display")
 
 let currentValue = '0';
-let workWithValue = 0;
+let storedValue = "";
+let currentOperator = null;
 
 Digits.forEach((digit) => {
     digit.addEventListener('click',()=>{
@@ -13,51 +15,70 @@ Digits.forEach((digit) => {
     })
 });
 
-function updateCurrentValue(digit){
-    if(currentValue.length<6){
-        if(currentValue === '0'){
-            currentValue = digit;
-        }
-        else{
-            currentValue += digit;
-        }
-        OutputDisplay.innerHTML = currentValue;
+function refreshOutputDisplay(){
+    OutputDisplay.innerHTML = currentValue;
+}
+
+function refreshUpperDisplay(){
+    if(storedValue||currentOperator){
+    UpperDisplay.innerHTML = `${storedValue} ${currentOperator}`;
+    }
+    else{
+        UpperDisplay.innerHTML = "";
     }
 }
 
+function updateCurrentValue(digit){
+    if(currentValue.length<6 || currentValue == '0'){
+    currentValue == '0' ? currentValue = digit : currentValue += digit;
+    refreshOutputDisplay()
+    }
+    // else{
+    //     check where the event is getting triggered from; if result:
+    //     currentValue = "flood"
+    //     refreshOutputDisplay()
+    // }
+}
 
 //Clear all display and holder variables
 AllClear.addEventListener('click',()=>{
-    currentValue = 0;
-    workWithValue = 0;
-    OutputDisplay.innerText  = currentValue;
+    currentValue = '0';
+    storedValue = "";
+    currentOperator = null;
+    refreshOutputDisplay()
+    refreshUpperDisplay()
 })
 
+Operators.forEach((operator)=>{
+    operator.addEventListener('click',()=>{
+        currentOperator = operator.innerText;
+        if(storedValue ===""){
+            storedValue = currentValue;
+        }
+        currentValue = '0'
+        refreshOutputDisplay();
+        refreshUpperDisplay();
 
+    })
+})
 
+Equals.addEventListener('click',()=>{
+    currentValue = calculate(storedValue, currentValue, currentOperator)
+    storedValue = "";
+    currentOperator = null;
+    refreshOutputDisplay()
+    refreshUpperDisplay()
 
+})
 
-
-
-
-
-
-
-
-
-
-function add(a,b){
-    return a+b;
-}
-
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
+function calculate(storedValue, currentValue, currentOperator){
+    if(storedValue === ""){return currentValue}
+    storedValue = parseInt(storedValue); currentValue = parseInt(currentValue)
+    console.log(storedValue)
+    switch (currentOperator){
+        case "+": return storedValue + currentValue;
+        case "−": return storedValue - currentValue;
+        case "×": return storedValue * currentValue;
+        case "/": return storedValue / currentValue;
+    }
 }
